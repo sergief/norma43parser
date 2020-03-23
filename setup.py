f486cc23c7ec021cce62cdf65bb1b2eaca7db85b
Sergi Espinar <sergief@users.noreply.github.com>
2020-03-23 23:01:24 +0100

    created configuration file for pypy aka pip

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name="norma43parser",
+    packages=["norma43parser"],
+    version="0.1",
+    license="MIT",
+    description="Parser for Bank Account information files formated in Norma 43",
+    author="Sergi Espinar",
+    author_email="sergief@users.noreply.github.com",
+    url="https://github.com/sergief/norma43parser",
+    download_url="https://github.com/sergief/norma43parser/archive/master.zip",
+    keywords=["norma43", "parser", "bank", "account", "n43", "csb"],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
