21ea831a782f2bef3110d2253cf9a7c7c4aec126
Sergi Espinar <sergief@users.noreply.github.com>
2020-04-08 12:00:16 +0200

    fixed packaging

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="norma43parser",
-    packages=["norma43parser"],
-    version="1.1",
+    version="1.1.2",
     license="MIT",
-    description="Parser for Bank Account information files formatted in Norma 43",
     author="Sergi Espinar",
     author_email="sergief@users.noreply.github.com",
+    description="Parser for Bank Account information files formatted in Norma 43",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/sergief/norma43parser",
-    download_url="https://github.com/sergief/norma43parser/archive/master.zip",
+    packages=setuptools.find_packages(),
     keywords=["norma43", "parser", "bank", "account", "n43", "csb"],
-    setup_requires=["wheel"],
-    install_requires=[],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
@@ -22,4 +24,5 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
+    python_requires=">=3.6",
 )
